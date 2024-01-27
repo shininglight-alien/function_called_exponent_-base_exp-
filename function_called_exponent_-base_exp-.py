d14b4e5d0e479612792e7ac720a857deaa16f92f
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-27 08:37:52 +0800

    final

--- function_called_exponent_-base_exp-.py
+++ function_called_exponent_-base_exp-.py
@@ -2,3 +2,7 @@
 
 def exponent(base, exp):
     return base ** exp
+
+# test the function
+print("2 raised to the power of 5 is:", exponent(2, 5))
+print("5 raised to the power of 4 is:", exponent(5, 4))
